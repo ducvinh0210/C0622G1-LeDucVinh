c6a6df26d7ab07ec533d645565b7497607076e27
unknown <you@example.com>
2022-07-10 21:10:38 +0700

    nop_bai_tap_bien_kieu_du_lieu_toan_tu

--- module 1/ss7_bien_kieu_du_lieu_toan_tu/bai_tap/ung_dung_chuyen_doi_tien_te.js
+++ module 1/ss7_bien_kieu_du_lieu_toan_tu/bai_tap/ung_dung_chuyen_doi_tien_te.js
@@ -1,22 +1,22 @@
 function changeMoney() {
-let Amount = document.getElementById("Amount").value;
-let From = document.getElementById("From Currency").value;
-let To = document.getElementById("To Currency").value;
-let Result;
+let amount = document.getElementById("Amount").value;
+let from = document.getElementById("From Currency").value;
+let to = document.getElementById("To Currency").value;
+let result;
 
-if (From == "USD"&& To == "VND"){
-    Result="Result: " + (Amount*23000)+ "VND";
+if (from == "USD"&& to == "VND"){
+    result="Result: " + (amount*23000)+ "VND";
 }
-else if(From == "VND"&& To == "USD"){
-    Result="Result: " + "$" + (Amount/23000);
+else if(from == "VND"&& to == "USD"){
+    result="Result: " + "$" + (amount/23000);
     }
-else if(From == "VND"){
-    Result="Result: " + Amount+" VND";
+else if(from == "VND"){
+    result="Result: " + amount+" VND";
 }
 else {
-Result="Result:" + " $" + Amount;
+result="Result:" + " $" + amount;
 }
-document.getElementById("Result").innerHTML=Result;
+document.getElementById("Result").innerHTML=result;
 
 
 }
