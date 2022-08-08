3fd5e57948244daa2728a52636102e6b4d0ca70b
unknown <you@example.com>
2022-08-08 23:56:07 +0700

    nb

--- module2/src/ss5_inheritance/exercise/class_point_and_moveablepoint/controller/MainController.java
+++ module2/src/ss5_inheritance/exercise/class_point_and_moveablepoint/controller/MainController.java
@@ -4,7 +4,7 @@ import ss5_inheritance.exercise.class_point_and_moveablepoint.model.MovablePoint
 
 public class MainController {
     public static void main(String[] args) {
-        MovablePoint movablePoint = new MovablePoint(3.4f,5.6f,4,4);
+        MovablePoint movablePoint = new MovablePoint(7.0f,2.5f,9,8);
         System.out.println(movablePoint);
         movablePoint.move() ;
         System.out.println(movablePoint);
