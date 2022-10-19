962c268f9c842e52aa46956028470a692d730aec
unknown <you@example.com>
2022-10-19 07:15:26 +0700

    nop_bai

--- module3/database/ss2_design_and_create_database/bai2_quan_ly_ban_hang.sql
+++ module3/database/ss2_design_and_create_database/bai2_quan_ly_ban_hang.sql
@@ -26,8 +26,10 @@ create table order_detail(
 id int ,
 id_product int,
 primary key (id,id_product),
+amount int,
 foreign key(id) references 	`order`(id),
 foreign key(id_product) references product(id_product)
+
 );
 
 insert into customer(name_customer,age) 
@@ -48,6 +50,16 @@ insert	into product(name_product,price_product)values
 ('Quat',1),
 ('Bep Dien',2);
 
+insert into order_detail(id,id_product,amount)
+values
+(1,1,3),
+(1,3,7),
+(1,4,2),
+(2,1,1),
+(3,1,8),
+(2,5,4),
+(2,3,3);
+
 
 -- Hiển thị các thông tin  gồm oID, oDate, oPrice của tất cả các hóa đơn trong bảng Order
 select 
@@ -57,8 +69,25 @@ select
 from `order`;
 
 -- Hiển thị danh sách các khách hàng đã mua hàng, và danh sách sản phẩm được mua bởi các khách hang
+select customer.name_customer,product.name_product from customer 
+inner join `order` on customer.id_customer= `order`.id_customer
+inner join order_detail on`order`.id= order_detail.id
+inner join product on order_detail.id_product= product.id_product;
 
-select 
+-- Hiển thị tên những khách hàng không mua bất kỳ một sản phẩm nào
+select * from customer where customer.id_customer not in (select id_customer from `order`);
+-- Hiển thị mã hóa đơn, ngày bán và giá tiền của từng hóa đơn 
+-- (giá một hóa đơn được tính bằng tổng giá bán của từng loại mặt hàng xuất hiện trong hóa đơn. Giá bán của từng loại được tính = odQTY*pPrice)
+
+select `order`.id,`order`.`date`,sum(order_detail.amount*product.price_product) as total_order_price
+from `order`
+inner join order_detail on `order`.id = order_detail.id
+inner join product on order_detail.id_product= product.id_product
+group by `order`.id;
+
+
+
+
 
 
 
