24df529e6207e69726d24dcf0d384ebfbd16bbac
unknown <you@example.com>
2022-11-28 10:28:56 +0700

    nop_bai

--- module4/bai7_spring_boot/blog/ss1_blog/src/main/java/com/example/model/controller/BlogerController.java
+++ module4/bai7_spring_boot/blog/ss1_blog/src/main/java/com/example/model/controller/BlogerController.java
@@ -16,49 +16,53 @@ import java.util.List;
 public class BlogerController {
     @Autowired
     private IBlogerService blogerService;
+
     @GetMapping("/create-blog")
-    public ModelAndView showCreateForm(){
-        ModelAndView modelAndView= new ModelAndView("/blog/create");
-modelAndView.addObject("blog" ,new Bloger());
-   return modelAndView;
-    }
-    @PostMapping("/create-blog")
-    public ModelAndView createBlog(@ModelAttribute("blog") Bloger bloger){
-        blogerService.save(bloger);
-        ModelAndView modelAndView=new ModelAndView("/blog/create");
-    modelAndView.addObject("blog",bloger);
-    modelAndView.addObject("message","Blog created successfully");
-    return modelAndView;
-    }
-    @GetMapping("/blogers")
-    public ModelAndView showBlogList(){
-        List<Bloger> blogers=blogerService.findAll();
-        ModelAndView modelAndView= new ModelAndView("/blog/index");
-   modelAndView.addObject("blogs",blogers);
-   return modelAndView;
-    }
-    @GetMapping("/edit-blog/{id}")
-    public ModelAndView showUpdateForm(@PathVariable int id){
-        Bloger bloger=blogerService.findById(id);
-        if (bloger!=null){
-            ModelAndView modelAndView= new ModelAndView("/blog/edit");
-            modelAndView.addObject("blog",bloger);
-            return modelAndView;
-        }else {
-            ModelAndView modelAndView= new ModelAndView("error.404");
+    public ModelAndView showCreateForm() {
+        ModelAndView modelAndView = new ModelAndView("/blog/create");
+        modelAndView.addObject("blog", new Bloger());
         return modelAndView;
+    }
+
+    @PostMapping("/create-blog")
+    public ModelAndView createBlog(@ModelAttribute("blog") Bloger bloger) {
+        blogerService.save(bloger);
+        ModelAndView modelAndView = new ModelAndView("/blog/create");
+        modelAndView.addObject("blog", bloger);
+        modelAndView.addObject("message", "Blog created successfully");
+        return modelAndView;
+    }
+
+    @GetMapping("/blogers")
+    public ModelAndView showBlogList() {
+        List<Bloger> blogers = blogerService.findAll();
+        ModelAndView modelAndView = new ModelAndView("/blog/index");
+        modelAndView.addObject("blogs", blogers);
+        return modelAndView;
+    }
+
+    @GetMapping("/edit-blog/{id}")
+    public ModelAndView showUpdateForm(@PathVariable int id) {
+        Bloger bloger = blogerService.findById(id);
+        if (bloger != null) {
+            ModelAndView modelAndView = new ModelAndView("/blog/edit");
+            modelAndView.addObject("blog", bloger);
+            return modelAndView;
+        } else {
+            ModelAndView modelAndView = new ModelAndView("error.404");
+            return modelAndView;
         }
 
     }
-@PostMapping("/edit-blog")
-    public ModelAndView update(@ModelAttribute("blog") Bloger bloger){
-        blogerService.save(bloger);
-        ModelAndView modelAndView= new ModelAndView("/blog/edit");
-        modelAndView.addObject("blog",bloger);
-        modelAndView.addObject("message","update blog successfully");
-        return modelAndView;
-}
 
+    @PostMapping("/edit-blog")
+    public ModelAndView update(@ModelAttribute("blog") Bloger bloger) {
+        blogerService.save(bloger);
+        ModelAndView modelAndView = new ModelAndView("/blog/edit");
+        modelAndView.addObject("blog", bloger);
+        modelAndView.addObject("message", "update blog successfully");
+        return modelAndView;
+    }
 
 
 }
