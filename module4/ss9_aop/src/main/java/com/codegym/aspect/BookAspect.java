6a69584777a838a180d083ba4eb1989dd1e2e01c
unknown <you@example.com>
2022-12-01 22:55:32 +0700

    nop_bai

--- module4/ss9_aop/src/main/java/com/codegym/aspect/BookAspect.java
+++ module4/ss9_aop/src/main/java/com/codegym/aspect/BookAspect.java
@@ -1,13 +1,36 @@
 package com.codegym.aspect;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterThrowing;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class BookAspect {
     private static long count;
     @AfterThrowing("execution(* com.codegym.controller.BookController.orderBook(..)")
-    public void checkOder(){
+    public void checkOrder(){
         System.out.println("___________________");
         System.out.println("không còn sách để mượn");
     }
+@After("execution(* com.codegym.controller.BookController.orderBook(..)")
+    public void longOrderDone(){
+    System.out.println("___________________________");
+    System.out.println("mượn sách thành công");
 
+}
+    @After("execution(* com.codegym.controller.BookController.returnBook(..)")
+    public void loggerPay(){
+        System.out.println("_________________");
+        System.out.println("Trả sách thành công");
+    }
+@After("execution(* com.codegym.controller.BookController.*(..))")
+    public void logAfterMethodBookController(JoinPoint joinPoint){
+        count++;
+        String nameMethod= joinPoint.getSignature().getName();
+    System.out.println("___________________________");
+    System.out.println("Người truy cập phương thức "+nameMethod+ "vào lúc "+ LocalDateTime.now());
+    System.out.println("Số lượng truy cập "+count);
+}
 }
