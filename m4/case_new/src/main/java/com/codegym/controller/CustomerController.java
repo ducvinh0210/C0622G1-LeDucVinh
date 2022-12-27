2fea4c6e899103caa580634a45dd3bf883c57362
unknown <you@example.com>
2022-12-27 21:53:31 +0700

    nb

--- m4/case_new/src/main/java/com/codegym/controller/CustomerController.java
+++ m4/case_new/src/main/java/com/codegym/controller/CustomerController.java
@@ -11,12 +11,15 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/customers")
@@ -29,7 +32,7 @@ public class CustomerController {
     ICustomerTypeService customerTypeService;
 
     @ModelAttribute("customerTypeList")
-    List<CustomerType> getCustomerType(){
+    List<CustomerType> getCustomerType() {
         return customerTypeService.findAll();
 
     }
@@ -74,9 +77,47 @@ public class CustomerController {
         }
     }
 
+    @GetMapping("/edit/{id}")
+    public ModelAndView editCustomer(@PathVariable int id) {
+        Optional<Customer> optionalCustomer = customerService.findById(id);
+        ModelAndView modelAndView = new ModelAndView("customer/edit");
+        if (!optionalCustomer.isPresent()) {
+            modelAndView.addObject("message", "Customer Not Found");
+            return modelAndView;
 
+        }
+        CustomerDto customerDto = new CustomerDto();
+        BeanUtils.copyProperties(optionalCustomer.get(), customerDto);
+        List<CustomerType> customerTypeList = customerTypeService.findAll();
+        modelAndView.addObject("customerDto", customerDto);
+        return modelAndView;
 
+    }
 
+    @PostMapping("/edit")
+    public ModelAndView acceptEditCustomer(@ModelAttribute @Validated CustomerDto customerDto, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            ModelAndView modelAndView = new ModelAndView("customer/edit");
+            return modelAndView;
+
+        } else {
+            Customer customer = new Customer();
+            BeanUtils.copyProperties(customerDto, customer);
+            customerService.save(customer);
+            ModelAndView modelAndView = new ModelAndView("customer/edit");
+            modelAndView.addObject("customerDto", customerDto);
+            modelAndView.addObject("message", "edit customer susscessfully");
+            return modelAndView;
+        }
+    }
+
+    @GetMapping("/delete")
+    public String deleteCustomer(@RequestParam(value = "idDelete") int id, RedirectAttributes redirectAttributes) {
+        customerService.remove(id);
+        redirectAttributes.addFlashAttribute("message", "Delete customer Successfully");
+        return "redirect:/customers";
+
+    }
 
 
 }
